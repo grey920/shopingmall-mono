6c55d96bf9f1c7e35addd5db20a5ab759f10c5e2
grey <charminggw@gmail.com>
2024-06-20 13:54:11 +0900

    feat(init): init2

---
+++ src/main/java/com/sparta/shoppingmallmono/ShoppingmallMonoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.shoppingmallmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallMonoApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( ShoppingmallMonoApplication.class, args );
+    }
+
+}
