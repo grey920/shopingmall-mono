ec89bdb47e34a39467153024953505cf73362ce6
grey <charminggw@gmail.com>
2024-06-22 18:12:46 +0900

    feat(login): CustomUserDetailsService에서 유저 정보 찾을때 이메일 암호화

--- src/main/java/com/sparta/shoppingmallmono/user/service/CustomUserDetailsService.java
+++ src/main/java/com/sparta/shoppingmallmono/user/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.sparta.shoppingmallmono.user.service;
 
+import com.sparta.shoppingmallmono.security.EncryptionUtil;
 import com.sparta.shoppingmallmono.user.domain.entity.User;
 import com.sparta.shoppingmallmono.user.domain.repository.UserRepository;
 import com.sparta.shoppingmallmono.user.web.request.CustomUserDetails;
@@ -15,12 +16,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final EncryptionUtil encryptionUtil;
 
     @Override
     public UserDetails loadUserByUsername( String username ) throws UsernameNotFoundException {
 
-        Optional<User> userData = userRepository.findByEmail( username );
+        // email은 DB에 암호화되어 저장되어 있다. 따라서 입력받은 username을 암호화하여 DB에서 찾아야 한다.
+        String encryptedUsername = encryptionUtil.encrypt( username );
 
+        Optional<User> userData = userRepository.findByEmail( encryptedUsername );
         return userData.map( CustomUserDetails::new ).orElse( null );
+
     }
 }
