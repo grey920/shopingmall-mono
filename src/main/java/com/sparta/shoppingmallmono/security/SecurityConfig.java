3e71158589425a97b14a80dd48ee5f7242b7489d
grey <charminggw@gmail.com>
2024-06-22 10:54:38 +0900

    fix(security): 외부에서 호출시 401 넘어가도록 filterChain 설정

--- src/main/java/com/sparta/shoppingmallmono/security/SecurityConfig.java
+++ src/main/java/com/sparta/shoppingmallmono/security/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.sparta.shoppingmallmono.security;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain( HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf( AbstractHttpConfigurer::disable )
+        ;
+
+        return httpSecurity.build();
     }
 
 }
