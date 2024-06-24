3a6e26414d53c738fcf463da92497a134dc04dff
grey <charminggw@gmail.com>
2024-06-24 09:55:03 +0900

    feat(login): 액세스 토큰 재발급 로직 서비스단으로 분리

--- src/main/java/com/sparta/shoppingmallmono/user/web/AuthController.java
+++ src/main/java/com/sparta/shoppingmallmono/user/web/AuthController.java
@@ -1,6 +1,7 @@
 package com.sparta.shoppingmallmono.user.web;
 
 import com.sparta.shoppingmallmono.security.jwt.JWTUtil;
+import com.sparta.shoppingmallmono.user.service.AuthService;
 import com.sparta.shoppingmallmono.user.web.request.UserLoginRequest;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final JWTUtil jwtUtil;
+    private final AuthService authService;
 
     @PostMapping("login")
     public String login( @Valid @RequestBody UserLoginRequest request ) {
@@ -37,52 +38,7 @@ public class AuthController {
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue( HttpServletRequest request, HttpServletResponse response) {
 
-        //get refresh token
-        String refresh = null;
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-
-            if (cookie.getName().equals("refresh")) {
-
-                refresh = cookie.getValue();
-            }
-        }
-
-        if (refresh == null) {
-
-            //response status code
-            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
-        }
-
-        //expired check
-        try {
-            jwtUtil.isExpired(refresh);
-        } catch ( ExpiredJwtException e) {
-
-            //response status code
-            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
-        }
-
-        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(refresh);
-
-        if (!category.equals("refresh")) {
-
-            //response status code
-            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
-        }
-
-        String username = jwtUtil.getUsername(refresh);
-        String role = jwtUtil.getRole(refresh);
-
-        //make new JWT
-        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
-        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
-
-        //response
-        response.setHeader("access", newAccess); // 새로운 액세스 토큰을 헤더에 담아서 보냄
-//        response.addCookie(createCookie("refresh", newRefresh));
-
-        return new ResponseEntity<>( HttpStatus.OK);
+        return authService.reissueToken(request, response);
     }
+
 }
